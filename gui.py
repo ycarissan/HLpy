992595afcb55d8a4387b676e01e263327e30064b
Yannick Carissan <ycarissan@gmail.com>
2024-05-27 09:37:58 +0200

    add spectrum frmae and canvas

--- gui.py
+++ gui.py
@@ -15,12 +15,12 @@ class HulisInterface(tk.Frame):
         center_panel (tkinter.Frame): Le panneau central de l'interface contenant le canvas de dessin.
         right_panel (tkinter.Canvas): Le panneau droit de l'interface.
         canvas (tkinter.Canvas): Le canvas de dessin des molécules.
-        molecule (Molecule): L'objet Molecule représentant la molécule dessinée.
+        dessinMolecule (DessinMolecule): L'objet Molecule représentant la molécule dessinée.
     """
 
     def __init__(self, master=None):
         """
-        Initialise un objet MoleculeDrawer.
+        Initialise un objet HulisInterface.
 
         Args:
             master (tkinter.Tk): La fenêtre principale de l'application.
@@ -29,39 +29,44 @@ class HulisInterface(tk.Frame):
         self.master = master
         self.pack(fill='both', expand=True)
         self.create_panels()
-        self.create_canvas()
         self.bind_events()
-        self.dessinMolecule = DessinMolecule(self.canvas)
+        self.dessinMolecule = DessinMolecule(self.canvas_molecule)
         self.drag_atom_at_startingpoint = None
+        self.drag_atom_at_endpoint = None
+        self.canvas_molecule = None
 
     def create_panels(self):
         """
-        Crée les panneaux gauche, central et droit de l'interface.
+        Crée les panneaux de l'interface.
         """
-        self.left_panel = tk.Canvas(self, bg='blue', width=int(self.master.winfo_width() * 0.2))
-        self.left_panel.pack(side='left', fill='both', expand=True)
-        
-        self.center_panel = tk.Frame(self)
-        self.center_panel.pack(side='left', fill='both', expand=True)
-        
-        self.right_panel = tk.Canvas(self, bg='orange', width=int(self.master.winfo_width() * 0.2))
-        self.right_panel.pack(side='left', fill='both', expand=True)
+        self.panneau_huckel   = tk.Frame(self, bg='blue',   width=int(self.master.winfo_width() * 0.1))
+        self.panneau_molecule = tk.Frame(self, bg='black')
+        self.panneau_spectre  = tk.Frame(self, bg='green',  width=int(self.master.winfo_width() * 0.1))
+        self.panneau_lewis    = tk.Frame(self, bg='orange', width=int(self.master.winfo_width() * 0.1))
 
-    def create_canvas(self):
-        """
-        Crée le canvas de dessin des molécules et lie les événements de clic.
-        """
-        self.canvas = tk.Canvas(self.center_panel, bg='white')
-        self.canvas.pack(fill='both', expand=True)
+        self.canvas_huckel   = tk.Canvas(self.panneau_huckel,   bg='lightblue')
+        self.canvas_molecule = tk.Canvas(self.panneau_molecule, bg='white')
+        self.canvas_spectre  = tk.Canvas(self.panneau_spectre,  bg='lightgreen')
+        self.canvas_lewis    = tk.Canvas(self.panneau_lewis,    bg='yellow')
+
+        self.panneau_huckel.pack(  side=tk.LEFT, fill=tk.BOTH)
+        self.panneau_molecule.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.panneau_spectre.pack( side=tk.LEFT, fill=tk.BOTH)
+        self.panneau_lewis.pack(   side=tk.LEFT, fill=tk.BOTH)
+
+        self.canvas_huckel.pack(  fill=tk.BOTH, expand=True)        
+        self.canvas_molecule.pack(fill=tk.BOTH, expand=True)
+        self.canvas_spectre.pack( fill=tk.BOTH, expand=True)
+        self.canvas_lewis.pack(   fill=tk.BOTH, expand=True)
 
     def bind_events(self):
         """
         Lie l'événement de touche 'l' pour afficher/masquer les labels des atomes.
         """
-        self.canvas.bind('<Button-1>', self.add_carbon_atom)
-        self.canvas.bind('<ButtonPress-1>', self.drag_start)
-        self.canvas.bind('<ButtonRelease-1>', self.drag_stop)
-        self.canvas.bind('<B1-Motion>', self.dragging)
+        self.canvas_molecule.bind('<Button-1>', self.add_carbon_atom)
+        self.canvas_molecule.bind('<ButtonPress-1>', self.drag_start)
+        self.canvas_molecule.bind('<ButtonRelease-1>', self.drag_stop)
+        self.canvas_molecule.bind('<B1-Motion>', self.dragging)
         self.master.bind('l', self.toggle_symbols)
         self.master.bind('q', self.quit_app)
 
@@ -100,6 +105,13 @@ class HulisInterface(tk.Frame):
         return self.dessinMolecule.add_carbon_atom(x, y)
     
     def add_bond(self, atome1, atome2):
+        """
+        Ajoute un lien entre deux atomes de la molécule.
+
+        Args:
+            atome1: Le premier atome.
+            atome2: Le deuxième atome.
+        """
         self.dessinMolecule.add_bond(atome1, atome2)
 
     def toggle_symbols(self, event):
@@ -127,6 +139,7 @@ def main():
     """
     root = tk.Tk()
     app = HulisInterface(root)
+    root.minsize(400, 300)
     root.geometry('800x600')
     root.mainloop()
 
